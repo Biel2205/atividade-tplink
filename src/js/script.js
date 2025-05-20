3abb50f020b6e602bb4652abe680290e6da8c29b
gabriel <fulanodetal@exemplo.br>
2025-05-19 22:19:54 -0300

    arumando login

---
+++ src/js/script.js
@@ -0,0 +1,21 @@
+
+var botaoEntrar 
+
+function login() {
+
+
+     var campoUsuário = document.getElementById("usuário").value
+
+    var campo = document.getElementById("").value
+
+    var campoSenha = document.getElementById("password").value
+
+   if (campoEmail == "adimin@adimin.com" && campoSenha == "adimin192" ) {
+    window.location.replace("dashboard.html")
+   }
+
+    console.log("entrou na função de login") 
+
+} else {
+   alert("")
+}
